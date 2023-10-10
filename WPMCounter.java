ceb56101698abc01cf3830bca5f6bbfda2e579d7
James Yin <96818980+xyinjame@users.noreply.github.com>
2023-10-10 15:31:45 -0500

    Add files via upload
    Updated program with the following features:
    - Accuracy now calculated based off of accuracy per character instead of per word
    - Added a new "Nightmare" difficulty option which randomizes the capitalization of the "hard" difficulty workbank

--- WPMCounter.java
+++ WPMCounter.java
@@ -2,85 +2,115 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 import java.time.LocalTime;
 import java.util.Random;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class WPMCounter {
-  public static double calculateAccuracy(List<String> presentedWords, String userTyped) {
-    String[] typedWords = userTyped.split(" ");
-    int correctCount = 0;
+  public static double calculateCharacterAccuracy(String presentedString, String userTyped) {
+    int minLength = Math.min(presentedString.length(), userTyped.length());
+    int errors = 0;
 
-    for (int i = 0; i < presentedWords.size() && i < typedWords.length; i++) {
-        if (presentedWords.get(i).equals(typedWords[i])) {
-            correctCount++;
-        }
+    for (int i = 0; i < minLength; i++) {
+      if (presentedString.charAt(i) != userTyped.charAt(i)) {
+        errors++;
+      }
     }
 
-    return (double) correctCount / presentedWords.size();
-}
-  
-  static String[] easyWords = {"hello", "how", "dog", "cat", "easy"};
-  static String[] mediumWords = {"elephant", "cashew", "medium", "wallet", "example"};
-  static String[] hardWords = {"Excruciating", "Religious", "Microphone", "Mischievous", "Xenotransplantation"};
-  
+    errors += Math.abs(presentedString.length() - userTyped.length()); // Add errors for
+                                                                       // extra/missing characters
+
+    return (1.0 - (double) errors / presentedString.length()) * 100; // Convert fraction to
+                                                                     // percentage
+  }
+
+  public static String randomizeCapitalization(String word, Random rand) {
+    StringBuilder randomizedWord = new StringBuilder();
+    for (char c : word.toCharArray()) {
+      boolean capitalize = rand.nextBoolean();
+      if (capitalize) {
+        randomizedWord.append(Character.toUpperCase(c));
+      } else {
+        randomizedWord.append(Character.toLowerCase(c));
+      }
+    }
+    return randomizedWord.toString();
+  }
+
+  static String[] easyWords = {"hello", "how", "dog", "cat", "easy", "pet", "iron", "gold", "zoo"};
+  static String[] mediumWords =
+      {"elephant", "cashew", "medium", "wallet", "example", "tissue", "paper", "towel", "bottle", "backpack"};
+  static String[] hardWords = {"Excruciating", "Religious", "Microphone", "Mischievous",
+      "Xenotransplantation", "Incomprehensibility", "Onomatopoeia", "Phenomenon", "Interdisciplinary", "Connoisseur"};
+
   public static void main(String[] args) throws InterruptedException {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Welcome to Words Per Minute Counter program.");
-    System.out.println("Please choose a difficulty level");
+    System.out.println("Please choose a difficulty level.");
     System.out.println("1: Easy");
     System.out.println("2. Medium");
     System.out.println("3. Hard");
+    System.out.println("4. Nightmare (Randomized capitalization)");
+
     Random rand = new Random();
     String input = scanner.nextLine().trim();
+
+    // Start countdown
+    System.out.println("");
     System.out.println("3");
     TimeUnit.SECONDS.sleep(1);
     System.out.println("2");
     TimeUnit.SECONDS.sleep(1);
     System.out.println("1");
     TimeUnit.SECONDS.sleep(1);
-    List<String> presentedWords = new ArrayList<>();
-    switch(input) {
-      case "1": 
+
+    StringBuilder presentedString = new StringBuilder();
+    switch (input) {
+      case "1":
         for (int i = 0; i < 10; i++) {
           String word = easyWords[rand.nextInt(easyWords.length)];
           System.out.print(word + " ");
-          presentedWords.add(word);
+          presentedString.append(word).append(" ");
         }
         break;
-      case "2": 
+      case "2":
         for (int i = 0; i < 10; i++) {
           String word = mediumWords[rand.nextInt(mediumWords.length)];
           System.out.print(word + " ");
-          presentedWords.add(word);
+          presentedString.append(word).append(" ");
         }
         break;
-      case "3": 
+      case "3":
         for (int i = 0; i < 10; i++) {
           String word = hardWords[rand.nextInt(hardWords.length)];
           System.out.print(word + " ");
-          presentedWords.add(word);
+          presentedString.append(word).append(" ");
+        }
+        break;
+      case "4":
+        for (int i = 0; i < 10; i++) {
+          String word = hardWords[rand.nextInt(hardWords.length)];
+          String randomizedWord = randomizeCapitalization(word, rand);
+          System.out.print(randomizedWord + " ");
+          presentedString.append(randomizedWord).append(" ");
         }
         break;
     }
     System.out.println();
-    
+
     double start = LocalTime.now().toNanoOfDay();
-    
+
     Scanner userScan = new Scanner(System.in);
     String typedWords = userScan.nextLine();
-    
+
     double end = LocalTime.now().toNanoOfDay();
     double elapsedTime = end - start;
     double seconds = elapsedTime / 1000000000.0;
     int numChars = typedWords.length();
     int wpm = (int) ((((double) numChars / 5) / seconds) * 60);
-    
-    double accuracy = calculateAccuracy(presentedWords, typedWords);
-    System.out.println("Your accuracy is " + (accuracy * 100) + "%");
+
+    double accuracy = calculateCharacterAccuracy(presentedString.toString().trim(), typedWords);
+    System.out.println("\nYour accuracy is " + accuracy + "%");
     System.out.println("Your calculated WPM is " + wpm);
-    
-   
+
+
   }
-  
+
 }
